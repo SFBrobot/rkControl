fc266f208e8c7f44fade5df87d03943b7c83dcbb
rk1024 <rschroeder@L7-Opt755-Tcher.sfhs.net>
2016-01-26 16:42:00 -0800

    Applied improvements from TBH code

--- pid.h
+++ pid.h
@@ -1,28 +1,29 @@
 typedef struct {
 	float deriv,
-		derivFlt,
 		err,
 		errThresh,
 		errXing,
 		input,
-		inLast,
 		integ,
 		integLim,
-		integXing,
-		kP, kI, kD,
+		kP,
+		kI,
+		kD,
 		setpoint,
-		tbhFactor,
 		thresh,
 		out;
 
-  bool doSgnLock,
-  	doRun,
-  	isOnTgt;
+	bool doSgnLock,
+		doRun,
+		doUpdate,
+		isOnTgt;
 } Pid;
 
-void setPidDFlt(Pid *pid, float flt) { pid->derivFlt = pow(.1, flt / 10 - 5); }
+bool isPidInThresh(Pid *pid, float thresh) { return fabs(pid->err) <= thresh; }
 
-bool isPidInThresh(Pid *pid, float thresh) { return abs(pid->err) <= thresh; }
+bool isPidIntegInThresh(Pid *pid, float thresh) { return fabs(pid->integ) <= thresh; }
+
+bool isPidDerivInThresh(Pid *pid, float thresh) { return fabs(pid->deriv) <= thresh; }
 
 void updatePidErr(Pid *pid) {
 	pid->err = pid->setpoint - pid->input;
@@ -30,25 +31,37 @@ void updatePidErr(Pid *pid) {
 	pid->isOnTgt = isPidInThresh(pid, pid->thresh);
 
 	if (pid->isOnTgt) pid->errThresh = 0;
-  else pid->errThresh = pid->err - pid->thresh * sgn(pid->err);
+	else pid->errThresh = pid->err - pid->thresh * sgn(pid->err);
 }
 
 void setPid(Pid *pid, float setpoint) {
 	pid->setpoint = setpoint;
-	pid->integXing = pid->errXing = 0;
+	pid->integ = 0;
 
 	updatePidErr(pid);
 }
 
 void resetPid(Pid *pid, float input) {
-	pid->input = pid->inLast = input;
-	pid->integ = 0;
+	pid->input = input;
+	pid->deriv = pid->integ = 0;
 
 	setPid(pid, input);
 }
 
-void initPid(Pid *pid, float thresh, float kP, float kI, float kD, float integLim, float derivFlt, bool doSgnLock) {
-	pid->deriv = 0;
+void setPidDoRun(Pid *pid, bool doRun) {
+	pid->doRun = doRun;
+
+	if (doRun) pid->doUpdate = true;
+}
+
+void initPid(
+	Pid *pid,
+	float thresh,
+	float kP,
+	float kI,
+	float kD,
+	float integLim,
+	bool doSgnLock) {
 	pid->integLim = integLim;
 	pid->kP = kP;
 	pid->kI = kI;
@@ -57,31 +70,31 @@ void initPid(Pid *pid, float thresh, float kP, float kI, float kD, float integLi
 	pid->out = 0;
 
 	pid->doSgnLock = doSgnLock;
-	pid->doRun = true;
+	pid->doUpdate = true;
 
-	setPidDFlt(pid, derivFlt);
 	resetPid(pid, 0);
+	setPidDoRun(pid, false);
 }
 
-float updatePid(Pid *pid, float input, float dt) {
+float updatePid(Pid *pid, float input, float deriv, float dt) {
 	pid->input = input;
+	pid->deriv = deriv;
 
 	updatePidErr(pid);
 
-	if (!pid->doRun) return pid->out = 0;
+	if (!pid->doRun) {
+		pid->doUpdate = false;
+		return pid->out = 0;
+	}
+
+	pid->integ = fmaxf(-pid->integLim, fminf(pid->integLim, pid->integ + pid->errThresh * pid->kI * dt));
 
 	if ((pid->errXing <= 0 && pid->err > 0) || (pid->errXing >= 0 && pid->err < 0)) {
 		pid->integ = 0;
-
 		pid->errXing = pid->err;
 	}
-	else 	pid->integ = fmaxf(-pid->integLim, fminf(pid->integLim, pid->integ + pid->kI * pid->err * dt));
 
-	pid->deriv += (1 - pow(.5, dt * pid->derivFlt)) * (pid->kD * (pid->inLast - pid->input) / dt - pid->deriv);
-
-	pid->inLast = pid->input;
-
-	pid->out = pid->kP * pid->errThresh + pid->integ + pid->deriv;
+	pid->out = pid->errThresh * pid->kP + pid->integ - pid->deriv * pid->kD;
 
 	if (pid->doSgnLock) {
 		if (pid->input > 0 && pid->out < 0) pid->out = 0;
