294fa2211026312fc6b1670ddfd86cb1857f7721
rookie1024 <rookie1286@gmail.com>
2016-03-08 17:02:07 -0800

    Remove quadratic kI
    It caused unnecessary fluctuation.

--- tbh.h
+++ tbh.h
@@ -9,7 +9,6 @@ typedef struct {
 		integXing,
 		kP,
 		kI,
-		kIQ,
 		kD,
 		setpoint,
 		thresh,
@@ -63,13 +62,11 @@ void initTbh(
 	float thresh,
 	float kP,
 	float kI,
-	float kIQ,
 	float kD,
 	float integLim,
 	bool doSgnLock) {
 	tbh->integLim = integLim;
 	tbh->kI = kI;
-	tbh->kIQ = kIQ;
 	tbh->kD = kD;
 	tbh->thresh = thresh;
 	tbh->out = 0;
@@ -79,19 +76,6 @@ void initTbh(
 
 	resetTbh(tbh, 0);
 	setTbhDoRun(tbh, false);
-}
-
-void initTbh(
-	Tbh *tbh,
-	float thresh,
-	float kP,
-	float kIStart,
-	float kIEnd,
-	float kIErr,
-	float kD,
-	float integLim,
-	bool doSgnLock) {
-	initTbh(tbh, thresh, kP, kIStart, (kIEnd - kIStart) / kIErr, kD, integLim, doSgnLock);
 }
 
 float updateTbh(Tbh *tbh, float input, float deriv, float dt) {
@@ -105,7 +89,7 @@ float updateTbh(Tbh *tbh, float input, float deriv, float dt) {
 		return tbh->out = 0;
 	}
 
-	tbh->integ = fmaxf(-tbh->integLim, fminf(tbh->integLim, tbh->integ + (tbh->errThresh * (tbh->kI + fabs(tbh->errThresh) * tbh->kIQ) - tbh->deriv * tbh->kD) * dt));
+	tbh->integ = fmaxf(-tbh->integLim, fminf(tbh->integLim, tbh->integ + (tbh->errThresh * tbh->kI - tbh->deriv * tbh->kD) * dt));
 
 	if ((tbh->errXing <= 0 && tbh->err > 0) || (tbh->errXing >= 0 && tbh->err < 0)) {
 		if (tbh->hasXed)
