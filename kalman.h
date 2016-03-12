a2ddd17b5a898aad15912c2d373e1934ca651c61
rookie1024 <rookie1286@gmail.com>
2016-03-12 09:57:29 -0800

    Ran files through Visual Studio's formatter

--- kalman.h
+++ kalman.h
@@ -1,33 +1,33 @@
 typedef struct {
-	float envNoise,
-		errCovar,
-		gain,
-		input,
-		out;
+  float envNoise,
+    errCovar,
+    gain,
+    input,
+    out;
 
 } KalmanFilter;
 
 typedef KalmanFilter KFlt;
 
 void resetKFlt(KFlt *flt, float value, float errCovar) {
-	flt->errCovar = errCovar;
-	flt->out = value;
+  flt->errCovar = errCovar;
+  flt->out = value;
 }
 
 void initKFlt(KFlt *flt, float envNoise) {
-	flt->envNoise = envNoise;
-	flt->gain = 0;
-	flt->input = 0;
+  flt->envNoise = envNoise;
+  flt->gain = 0;
+  flt->input = 0;
 
-	resetKFlt(flt, 0, 0);
+  resetKFlt(flt, 0, 0);
 }
 
 void updateKFlt(KFlt *flt, float input, float dt) {
-	flt->input = input;
+  flt->input = input;
 
-	flt->gain = flt->errCovar / (flt->errCovar + flt->envNoise);
+  flt->gain = flt->errCovar / (flt->errCovar + flt->envNoise);
 
-	flt->out = flt->out + flt->gain * (flt->input - flt->out);
+  flt->out = flt->out + flt->gain * (flt->input - flt->out);
 
-	flt->errCovar *= (1 - flt->gain);
+  flt->errCovar *= (1 - flt->gain);
 }
--- tbh.h
+++ tbh.h
@@ -1,24 +1,26 @@
 typedef struct {
-	float deriv,
-		err,
-		errThresh,
-		errXing,
-		input,
-		integ,
-		integLim,
-		integXing,
-		kP,
-		kI,
-		kD,
-		setpoint,
-		thresh,
-		out;
+  float deriv,
+    err,
+    errThresh,
+    errXing,
+    input,
+    integ,
+    integLim,
+    integXing,
+    kP,
+    kI,
+    kD,
+    kDCross,
+    setpoint,
+    thresh,
+    out;
 
-	bool doSgnLock,
-		hasXed,
-		doRun,
-		doUpdate,
-		isOnTgt;
+  bool doSgnLock,
+    doDerivSgnLock,
+    hasXed,
+    doRun,
+    doUpdate,
+    isOnTgt;
 } Tbh;
 
 bool isTbhInThresh(Tbh *tbh, float thresh) { return fabs(tbh->err) <= thresh; }
@@ -26,88 +28,121 @@ bool isTbhInThresh(Tbh *tbh, float thresh) { return fabs(tbh->err) <= thresh; }
 bool isTbhDerivInThresh(Tbh *tbh, float thresh) { return fabs(tbh->deriv) <= thresh; }
 
 void updateTbhErr(Tbh *tbh) {
-	tbh->err = tbh->setpoint - tbh->input;
+  tbh->err = tbh->setpoint - tbh->input;
 
-	tbh->isOnTgt = isTbhInThresh(tbh, tbh->thresh);
+  tbh->isOnTgt = isTbhInThresh(tbh, tbh->thresh);
 
-	if (tbh->isOnTgt) tbh->errThresh = 0;
-	else tbh->errThresh = tbh->err - tbh->thresh * sgn(tbh->err);
+  if (tbh->isOnTgt) tbh->errThresh = 0;
+  else tbh->errThresh = tbh->err - tbh->thresh * sgn(tbh->err);
 }
 
 void setTbh(Tbh *tbh, float setpoint) {
-	tbh->setpoint = setpoint;
-	tbh->integXing = 0;
-	tbh->hasXed = false;
+  tbh->setpoint = setpoint;
+  tbh->integXing = 0;
+  tbh->hasXed = false;
 
-	updateTbhErr(tbh);
+  updateTbhErr(tbh);
 
-	tbh->errXing = tbh->err; //Don't immediately trigger a crossing
+  tbh->errXing = tbh->err; //Don't immediately trigger a crossing
 }
 
 void resetTbh(Tbh *tbh, float input) {
-	tbh->input = input;
-	tbh->deriv = tbh->integ = 0;
+  tbh->input = input;
+  tbh->deriv = tbh->integ = 0;
 
-	setTbh(tbh, input);
+  setTbh(tbh, input);
 }
 
 void setTbhDoRun(Tbh *tbh, bool doRun) {
-	tbh->doRun = doRun;
+  tbh->doRun = doRun;
 
-	if (doRun) tbh->doUpdate = true;
+  if (doRun) tbh->doUpdate = true;
 }
 
 void initTbh(
-	Tbh *tbh,
-	float thresh,
-	float kP,
-	float kI,
-	float kD,
-	float integLim,
-	bool doSgnLock) {
-	tbh->integLim = integLim;
-	tbh->kI = kI;
-	tbh->kD = kD;
-	tbh->thresh = thresh;
-	tbh->out = 0;
+  Tbh *tbh,
+  float thresh,
+  float kP,
+  float kI,
+  float kD,
+  float kDCross,
+  float integLim,
+  bool doSgnLock) {
+  tbh->integLim = integLim;
+  tbh->kI = kI;
+  tbh->kD = kD;
+  tbh->kDCross = kDCross;
+  tbh->thresh = thresh;
+  tbh->out = 0;
 
-	tbh->doSgnLock = doSgnLock;
-	tbh->doUpdate = true;
+  tbh->doSgnLock = doSgnLock;
+  tbh->doDerivSgnLock = kDCross != kD;
+  tbh->doUpdate = true;
 
-	resetTbh(tbh, 0);
-	setTbhDoRun(tbh, false);
+  resetTbh(tbh, 0);
+  setTbhDoRun(tbh, false);
+}
+
+void initTbh(
+  Tbh *tbh,
+  float thresh,
+  float kP,
+  float kI,
+  float kD,
+  float integLim,
+  bool doSgnLock) {
+
+  initTbh(tbh, thresh, kP, kI, kD, kD, integLim, doSgnLock);
 }
 
 float updateTbh(Tbh *tbh, float input, float deriv, float dt) {
-	tbh->input = input;
-	tbh->deriv = deriv;
+  tbh->input = input;
+  tbh->deriv = deriv;
 
-	updateTbhErr(tbh);
+  updateTbhErr(tbh);
 
-	if (!tbh->doRun) {
-		tbh->doUpdate = false;
-		return tbh->out = 0;
-	}
+  if (!tbh->doRun) {
+    tbh->doUpdate = false;
+    return tbh->out = 0;
+  }
 
-	tbh->integ = fmaxf(-tbh->integLim, fminf(tbh->integLim, tbh->integ + (tbh->errThresh * tbh->kI - tbh->deriv * tbh->kD) * dt));
+  if (tbh->doDerivSgnLock) {
+    if (tbh->err > 0 && tbh->deriv > 0) tbh->deriv;
+    else if (tbh->err < 0 && tbh->deriv < 0) tbh->deriv = 0;
+  }
 
-	if ((tbh->errXing <= 0 && tbh->err > 0) || (tbh->errXing >= 0 && tbh->err < 0)) {
-		if (tbh->hasXed)
-			tbh->integ = (tbh->integ + tbh->integXing) / 2.;
-		else
-			tbh->hasXed = true;
+  tbh->integ = fmaxf(
+    -tbh->integLim,
+    fminf(
+      tbh->integLim,
+      tbh->integ + (
+        tbh->errThresh * tbh->kI
+        - tbh->deriv * (
+          (tbh->err > 0 && tbh->deriv > 0) || (tbh->err < 0 && tbh->deriv < 0)
+          ? tbh->kDCross
+          : tbh->kD
+          )
+        ) * dt
+      )
+    );
 
-		tbh->integXing = tbh->integ;
+  if ((tbh->errXing <= 0 && tbh->err > 0) || (tbh->errXing >= 0 && tbh->err < 0)) {
+    if (tbh->hasXed)
+      tbh->integ = (tbh->integ + tbh->integXing) / 2.;
+    else
+      tbh->hasXed = true;
 
-		tbh->errXing = tbh->err;
-	}
+    tbh->integXing = tbh->integ;
 
-	tbh->out = tbh->integ + tbh->errThresh * tbh->kP;
+    tbh->errXing = tbh->err;
+  }
 
-	if (tbh->doSgnLock) {
-		if (tbh->input > 0 && tbh->out < 0) tbh->integ = tbh->out = 0;
-		else if (tbh->input < 0 && tbh->out > 0) tbh->integ = tbh->out = 0;
-	}
+  tbh->out = tbh->integ + tbh->errThresh * tbh->kP;
 
-	return tbh->out;
+  if (tbh->doSgnLock) {
+    if (tbh->input > 0 && tbh->out < 0) tbh->integ = tbh->out = 0;
+    else if (tbh->input < 0 && tbh->out > 0) tbh->integ = tbh->out = 0;
+  }
+
+  return tbh->out;
 }
