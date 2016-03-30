5e32bb260f9a6f516cc0747086d4e64294fd9a54
rk1024 <rookie1286@gmail.com>
2016-03-29 17:46:14 -0700

    Added simple guess for TBH algorithm

--- tbh.h
+++ tbh.h
@@ -34,9 +34,12 @@ void updateTbhErr(Tbh *tbh) {
 }
 
 void setTbh(Tbh *tbh, float setpoint) {
+	tbh->hasXed = tbh->setpoint != 0;
+
+	if (tbh->hasXed) tbh->integXing *= setpoint / tbh->setpoint; //Take a linear guess
+	else tbh->integXing = 0;
+
 	tbh->setpoint = setpoint;
-	tbh->integXing = 0;
-	tbh->hasXed = false;
 
 	updateTbhErr(tbh);
 
