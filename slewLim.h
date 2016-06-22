2b32f5ad9023f88212a6e10123cf627c853dbe22
l4cuatro <lpieroni@sfhighschool.net>
2016-06-21 22:43:25 -0700

    Overhauled it into one function, should work now

--- slewLim.h
+++ slewLim.h
@@ -1,28 +1,15 @@
 #ifndef SLEW_LIM
 #define SLEW_LIM
 
-typedef struct {
-  float val,
-    valLast,
-    max;
-} SlewLimiter;
-
-typedef SlewLimiter SLim;
-
-void initSLim(SLim *lim, float maxSlew) {
-  lim->max = maxSlew;
+int mtrSlew(nMotors mtr, int target, int maxSlew) {
+	if(abs(target - motor[mtr]) > maxSlew) {
+		if(target > motor[mtr])
+			return motor[mtr] += maxSlew;
+		else
+			return motor[mtr] -= maxSlew;
+	}
+	else
+		return motor[mtr] = target;
 }
 
-float setSLim(SLim *lim, float setPoint) {
-  lim->valLast = lim->val;
-  lim->val = setPoint;
-  if(fabs(lim->val - lim->valLast) > lim->max) {
-    if(lim->val > lim->valLast)
-      return lim->val = lim->valLast + lim->max;
-    else
-      return lim->val = lim->valLast - lim->max;
-  }
-  else
-  	return lim->val;
-}
 #endif
